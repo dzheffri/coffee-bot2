dfd449ba59783128021766a3bc9fdd669039a13e
dzheffri <madykadzheffri@gmail.com>
2026-03-20 15:08:22 +0200

    Use environment variable for BOT_TOKEN
    Replace hardcoded TOKEN with environment variable

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
-TOKEN = "8671987939:AAEjtckBobJ52yR-gbbquq645NGQyENGkfI"
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
 SUPER_ADMIN_IDS = [566408696]
+
+if not TOKEN:
+    raise ValueError("Не найден BOT_TOKEN в переменных окружения")
