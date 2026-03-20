fedf4d4c590ac58121d4f3de404226785472969d
dzheffri <madykadzheffri@gmail.com>
2026-03-20 16:01:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
 SUPER_ADMIN_IDS = [566408696]
 
 if not TOKEN:
     raise ValueError("Переменная окружения BOT_TOKEN не задана")
+
+if not DATABASE_URL:
+    raise ValueError("Переменная окружения DATABASE_URL не задана")
