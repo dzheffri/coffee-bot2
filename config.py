d92179197e51672e44d6995ff17ed7f670a7fd5c
dzheffri <madykadzheffri@gmail.com>
2026-03-20 15:19:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ TOKEN = os.getenv("BOT_TOKEN")
 SUPER_ADMIN_IDS = [566408696]
 
 if not TOKEN:
-    raise ValueError("Не найден BOT_TOKEN в переменных окружения")
+    raise ValueError("Переменная окружения BOT_TOKEN не задана")
